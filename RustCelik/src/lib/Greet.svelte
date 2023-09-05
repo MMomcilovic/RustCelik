cecab75bcaad281872d8b40e5c26e4745fcecb9d
MMomcilovic <miladin.momcilovic@gmail.com>
2023-09-05 14:07:43 +0200

    Initialize Tauri and Svelte

---
+++ RustCelik/src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greeting = '';
+
+	async function greet() {
+		greeting = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter your name" bind:value={name} />
+	<button on:click={greet}>Greet</button>
+	<p>{greeting}</p>
+</div>
