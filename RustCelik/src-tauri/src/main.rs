9a290a3968a1b0a542fa5ee9fd042369214428a9
MMomcilovic <miladin.momcilovic@gmail.com>
2023-09-10 01:34:36 +0200

    Improved loop logic

--- RustCelik/src-tauri/src/main.rs
+++ RustCelik/src-tauri/src/main.rs
@@ -5,7 +5,6 @@ use idreader_lib::module_reader::reader::PersonalId;
 use pcsc::*;
 use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, Window};
 use tauri_plugin_positioner::{Position, WindowExt};
-use viuer::Config;
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
@@ -66,38 +65,64 @@ fn main() {
 }
 
 fn card_info(window: Window) {
-    let mut old_readers = 0;
-    let mut info_sent = false;
-    let mut counter = 0;
+    let mut card_read = false;
+    let mut reader_states = vec![ReaderState::new(PNP_NOTIFICATION(), State::UNAWARE)];
+    let ctx = match Context::establish(Scope::User) {
+        Ok(ctx) => ctx,
+        Err(_err) => {
+            window.emit("card_info", "Error").unwrap();
+            return;
+        }
+    };
     std::thread::spawn(move || loop {
-        counter += 1;
         std::thread::sleep(std::time::Duration::from_secs(1));
-        let ctx = match Context::establish(Scope::User) {
-            Ok(ctx) => ctx,
+        fn is_dead(rs: &ReaderState) -> bool {
+            rs.event_state().intersects(State::UNKNOWN | State::IGNORE)
+        }
+        fn disconnected(rs: &ReaderState) -> bool {
+            rs.event_state().intersects(State::EMPTY)
+        }
+        for rs in &reader_states {
+            if is_dead(rs) {
+                println!("Removing {:?}", rs.name());
+            } else if disconnected(rs) {
+                card_read = false;
+            }
+        }
+        reader_states.retain(|rs| !is_dead(rs));
+
+        if ctx.list_readers_len().unwrap() < 40 {
+            window.emit("card_info", "No reader found").unwrap();
+            card_read = false;
+            continue;
+        }
+        let mut readers_buf = [0; 2048];
+
+        let readers = match ctx.list_readers(&mut readers_buf) {
+            Ok(readers) => readers,
             Err(err) => {
-                eprintln!("Failed to establish context: {}", err);
+                eprintln!("Failed to list readers: {}", err);
+                window.emit("card_info", "No reader found").unwrap();
+                card_read = false;
                 continue;
             }
         };
-        if counter > 30 {
-            info_sent = false;
-            counter = 0;
+        for reader in readers {
+            if !reader_states.iter().any(|rs| rs.name() == reader) {
+                println!("Adding {:?}", reader);
+                reader_states.push(ReaderState::new(reader, State::UNAWARE));
+            }
         }
-        if ctx.list_readers_len().unwrap() < 40 {
-            window.emit("card_info", "No reader found").unwrap();
-            continue;
+        for rs in &mut reader_states {
+            rs.sync_current_state();
         }
-        if old_readers == ctx.list_readers_len().unwrap() && info_sent {
-            continue;
-        }
-        old_readers = ctx.list_readers_len().unwrap();
-        let mut readers_buf = [0; 2048];
 
         let mut readers = match ctx.list_readers(&mut readers_buf) {
             Ok(readers) => readers,
             Err(err) => {
                 eprintln!("Failed to list readers: {}", err);
                 window.emit("card_info", "No reader found").unwrap();
+                card_read = false;
                 continue;
             }
         };
@@ -109,108 +134,54 @@ fn card_info(window: Window) {
                 continue;
             }
         };
+        if reader_states.len() > 1 && !card_read {
+            // Connect to the card.
+            let result = ctx.connect(reader, ShareMode::Shared, Protocols::ANY);
+            let card = match result {
+                Ok(card) => card,
+                Err(Error::NoSmartcard) => {
+                    println!("A smartcard is not present in the reader.");
+                    window.emit("card_info", "No card inserted").unwrap();
+                    card_read = false;
+                    continue;
+                }
+                Err(Error::RemovedCard) => {
+                    println!("The card was removed before we could read it.");
+                    window.emit("card_info", "No card inserted").unwrap();
+                    card_read = false;
+                    continue;
+                }
+                Err(err) => {
+                    eprintln!("Failed to connect to card: {}", err);
+                    continue;
+                }
+            };
 
-        // Connect to the card.
-        let result = ctx.connect(reader, ShareMode::Shared, Protocols::ANY);
-        let card = match result {
-            Ok(card) => card,
-            Err(Error::NoSmartcard) => {
-                println!("A smartcard is not present in the reader.");
-                window.emit("card_info", "No card inserted").unwrap();
-                continue;
-            }
-            Err(Error::RemovedCard) => {
-                println!("The card was removed before we could read it.");
-                window.emit("card_info", "No card inserted").unwrap();
-                continue;
-            }
-            Err(err) => {
-                eprintln!("Failed to connect to card: {}", err);
-                continue;
-            }
-        };
-
-        let mut personal_id = PersonalId::new(&card).unwrap();
-        personal_id.read_id(&card).unwrap();
-
-        let conf = Config {
-            absolute_offset: false,
-            x: 0,
-            y: 0,
-            width: Some(42),
-            height: Some(28),
-            ..Default::default()
-        };
-        let img = image::load_from_memory(&personal_id.image).expect("Could not be read");
-
-        viuer::print(&img, &conf).expect("Image printing failed.");
-        for (_tag, item) in personal_id.personal.iter() {
-            println!("{}", item);
+            let mut personal_id = PersonalId::new(&card).unwrap();
+            personal_id.read_id(&card).unwrap_or_else(|_| {
+                let _ = window.emit("card_info", "Error while reading card!");
+            });
+            card_read = true;
+            println!("Sending card info to frontend");
+            window
+                .emit(
+                    "card_info",
+                    serde_json::from_str::<serde_json::Value>(&personal_id.to_json())
+                        .unwrap()
+                        .to_string(),
+                )
+                .unwrap();
         }
-        println!(
-            "{:?}",
-            serde_json::from_str::<serde_json::Value>(&personal_id.to_json())
-        );
-        info_sent = true;
+        // Wait until the state changes.
+        ctx.get_status_change(None, &mut reader_states)
+            .expect("failed to get status change");
 
-        window
-            .emit(
-                "card_info",
-                serde_json::from_str::<serde_json::Value>(&personal_id.to_json())
-                    .unwrap()
-                    .to_string(),
-            )
-            .unwrap();
+        // Print current state.
+        println!();
+        for rs in &reader_states {
+            if rs.name() != PNP_NOTIFICATION() {
+                println!("{:?} {:?} {:?}", rs.name(), rs.event_state(), rs.atr());
+            }
+        }
     });
 }
-
-// fn look_for_readers() {
-//     // Establish a PC/SC context.
-//     let ctx = match Context::establish(Scope::User) {
-//         Ok(ctx) => ctx,
-//         Err(err) => {
-//             eprintln!("Failed to establish context: {}", err);
-//             return;
-//         }
-//     };
-//     let mut old_readers = 0;
-//     loop {
-//         // List available readers.
-//         let mut readers_buf = [0; 2048];
-//         match ctx.list_readers_len() {
-//             Ok(len) => {
-//                 if len == old_readers {
-//                     std::thread::sleep(std::time::Duration::from_secs(1));
-//                     continue;
-//                 }
-//                 old_readers = len;
-//             }
-//             Err(err) => {
-//                 eprintln!("Failed to list readers: {}", err);
-//                 continue;
-//             }
-//         }
-//         let mut readers = match ctx.list_readers(&mut readers_buf) {
-//             Ok(readers) => readers,
-//             Err(err) => {
-//                 eprintln!("Failed to list readers: {}", err);
-//                 continue;
-//             }
-//         };
-
-//         if old_readers == 0 {
-//             println!("No smart card readers found.");
-//         } else {
-//             println!("Smart card readers:");
-//             loop {
-//                 let reader = match readers.next() {
-//                     Some(reader) => reader,
-//                     None => {
-//                         break;
-//                     }
-//                 };
-//                 println!("{:?}", reader);
-//             }
-//         }
-//     }
-// }
